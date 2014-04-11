825e795df54ebb74e3c4e93878a6517d0e7dbe0d
Jae Choi <jaekun.choi@gmail.com>
2014-04-11 07:59:33 +0000

    added performance benchmark for testing HHVM

---
+++ web/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    $startTime = microtime(true);
+    $counter=0;
+    while($counter == 1000000000000){
+        serialize(git $name);
+        $counter++;
+    }
+    $endTime = microtime(true);
+    $elapsed = $endTime - $startTime;
+    echo "Start: $startTime<br>";
+    echo "End: $endTime<br>";
+    return "Execution time : $elapsed seconds";
+
+//    return 'Hello '.$app->escape($name);
+});
+
+$app->run();
